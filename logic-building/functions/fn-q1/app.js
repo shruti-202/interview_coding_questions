8b934d5706b5145f365e7dc289066aedf780fa8e
Shruti <shrutigawande1910@gmail.com>
2025-05-27 17:02:25 +0530

    feat: function - q1

---
+++ logic-building/functions/fn-q1/app.js
@@ -0,0 +1,10 @@
+const findValue = (a, b) => {
+  let result = Math.pow(a, b);
+  return result;
+};
+
+console.log(findValue(2, 3));
+
+const getValue = (a, b) => a ** b;
+
+console.log(getValue(2, 5));
